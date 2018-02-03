fc980ddff1ffcf737c05109edeb9e2c42cfcc01e
ChiShengChen <m50816m50816@gmail.com>
2018-02-04 07:18:03 +0800

    soundsensor for arduino 101

---
+++ sketch_feb03c.ino
@@ -0,0 +1,13 @@
+ int sp = A5;
+
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ pinMode(A5,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+Serial.println(analogRead(A5));
+}
